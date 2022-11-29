7e002577858edf42fe23d930e87bcc92b9b60593
samirametin <samire.mahmudova1996@gmail.com>
2022-11-29 20:54:53 +0400

    svg changes

--- main.js
+++ main.js
@@ -33,8 +33,8 @@
     
     }
 })
-const upSort=document.querySelector(".sort-icon svg:firstChild")
-const downSort=document.querySelector(".sort-icon svg:lastChild")
+const upSort=document.querySelector(".sort_icon svg:firstChild")
+const downSort=document.querySelector(".sort_icon svg:lastChild")
 
 downSort.addEventListener("click",function(){
     this.style.display="none";
