f3061adaa424d55562ac6e32605d8a90f25de226
samirametin <samire.mahmudova1996@gmail.com>
2022-11-29 05:10:37 +0400

    test add

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
  let button=document.querySelector(".addbtn");
  let i=0;
+ let task1=document.querySelector(".task1");
+ const texts=[];
  button.addEventListener("click",function(){
-    if (i<4){
+    if (i<5){
     let inputText=document.querySelector("input");
     let tasks=document.querySelector(".tasks")
     let div=document.createElement("div");
@@ -13,8 +15,22 @@
     let img=document.createElement("img");
     div.append(img)
     img.setAttribute("src","/img/Group 77.jpg")
+    img.classList.add("remove")
     inputText.value="";
     i=i+1;
+    if (i==5){
+        task1.style.display="none";
+    }else{
+        task1.style.display="flex"
+    }
+    texts.push(pFile.innerText)
+    
 }
 })
-let remove=document.querySelector("imimg")
+// let remove=document.querySelector(".remove")
+// remove.addEventListener("click",function(e){
+//     let removeImage=e.target;
+//     let mustRemove=removeImage.parentElement;
+//     mustRemove.remove();
+// })
+console.log(texts.sort((a,b)=>{a>b}))
